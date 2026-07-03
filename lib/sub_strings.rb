c7b7614f19bcd2d7f92bb444ce54ade4e03a65e9
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-07-03 10:05:25 +0200

    Cleaned up code and added comments for the thought process and functionality of each line.

--- lib/sub_strings.rb
+++ lib/sub_strings.rb
@@ -10,55 +10,31 @@ def sub_string_finder(string, dictionary)
   dictionary.each do |word|
     # For each word in the dictionary
     word_array = word.chars
-
-
-    puts "=========="
-    puts "THE WORD IS #{word}"
-    print word_array
-    puts " "
-
     # Go through each character in the string array
     string_array.each_with_index do |string_char, index|
-
-      puts "**********"
-      puts "string_char is #{string_char} at index #{index}"
-
       # Slice out the relevant part of the string array
-      string_array_slice = string_array.slice(index, string_array.length)
-      puts "string_array_slice"
-      print string_array_slice
-      puts " "
+      string_array_slice = string_array.slice
 
       if string_array_slice.length < word_array.length
+        # If the string slice is shorter than the word, skip
         break
       else
         word_array.each_with_index do |word_char, index|
-          puts "word_char at index #{index} is #{word_char}"
-          puts "string_array_slice at index #{index} is #{string_array_slice[index]}"
 
           if index == (word_array.length - 1) && word_char == string_array_slice[index]
-            puts "Add word, results hash ="
+            # If it's the last character of the word and it matches the character in the string at the same index, add it tou results
             results_hash[word] += 1
-            print results_hash
-            puts " "
           elsif word_char == string_array_slice[index]
-            puts "word_char == string_array_slice[index] and it is true? #{word_char == string_array_slice[index]}"
+            # If the character in the word and character in the string at the same positions are the same, keep going
           else
-            puts "else!"
+            # If they aren't, end the loop
             break
           end
           
         end
       end
-
-    # end of one iteration of string_array.each_with_index
-    puts "----------"
     end
-    # end of one iteration of dictionary do 
-    puts ".........."
   end
-  puts "final results_hash is"
-  print results_hash
   return results_hash
 end
 
@@ -68,6 +44,3 @@ end
 # Go through string_array one by one while matching word_array
 # If we have finishied word_array and there's a perfect match with string_array, push word into a hash and increase the count by 1
 # We still have to go through every item in the string_array to make sure that we catch everything
-
-
-# 
